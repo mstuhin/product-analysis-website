c092149347c72e7b757774f0aecc6d5a0ccaafda
unknown <mstuhin>
2022-04-05 00:12:06 +0600

    secondcomit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Chooses your smartwatch</h1>
+
+
+            <p>A smart home will not only show you time, it will also make you smart.</p>
+
+        </div>
+    );
+};
+
+export default Home;
